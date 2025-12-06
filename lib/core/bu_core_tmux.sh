d98d0c8641fa03a2c11bb3b1a6691382774b22fd
Sun Jiacheng <sunjc826@gmail.com>
2025-12-06 13:28:42 +0800

    Collapse functions into 1

--- lib/core/bu_core_tmux.sh
+++ lib/core/bu_core_tmux.sh
@@ -18,15 +18,16 @@ __bu_spawn_tmux_header()
 __bu_spawn_tmux_resolve_split_mode()
 {
     local split_mode=$1
+    local -a tmux_args
     case "$split_mode" in
     none)
-        BU_RET=(new-window -d)
+        tmux_args=(new-window -d)
         ;;
     vertical)
-        BU_RET=(split-window -vd -t "$TMUX_PANE")
+        tmux_args=(split-window -vd -t "$TMUX_PANE")
         ;;
     horizontal)
-        BU_RET=(split-window -hd -t "$TMUX_PANE")
+        tmux_args=(split-window -hd -t "$TMUX_PANE")
         ;;
     *)
         bu_log_err "Bad split mode[$split_mode]"
@@ -35,61 +36,9 @@ __bu_spawn_tmux_resolve_split_mode()
     esac
     # Default format: #{session_name}:#{window_index}
     # But window index is not stable, so we should a custom stable format #{pane_id}
-    BU_RET+=(-P -F '#{pane_id}')
-}
+    tmux_args+=(-P -F '#{pane_id}')
 
-__bu_spawn_tmux_joinable_header()
-{
-    local command=$1
-    local tmp_file=$2
-    local ret_file=$tmp_file.ret
-    local fifo_file=$tmp_file.fifo
-    echo 'rm '"$tmp_file"
-    __bu_spawn_tmux_header "$command"
-    # echo 1 first to account for possible Ctrl-C
-    echo 'echo 1 >'"$ret_file"
-    echo 'exec {RET_FILE_FD}>'"$ret_file"
-    echo 'rm '"$ret_file"
-    echo 'bu_exit_handler_setup_no_e'
-    echo 'bu_scope_push'
-    # shellcheck disable=SC2016
-    echo 'bu_sync_acquire_fd "$RET_FILE_FD"'
-    # shellcheck disable=SC2016
-    echo 'bu_scope_cleanup bu_release_fd "$RET_FILE_FD"'
-    echo 'echo >'"$fifo_file"
-}
-
-__bu_spawn_tmux_joinable_footer()
-{
-    echo
-    # shellcheck disable=SC2016
-    echo 'echo $? >&"$RET_FILE_FD"'
-    echo 'bu_scope_pop'
-}
-
-__bu_spawn_tmux_joinable_driver()
-{
-    local tmp_file=$1
-    local split_mode=$2
-    local driver_file=$tmp_file.driver.sh
-    local fifo_file=$tmp_file.fifo
-    {
-        echo 'rm '"$driver_file"
-        echo . /etc/profile
-        echo . "$HOME"/.bashrc
-        echo bash "$tmp_file"
-    } > "$driver_file"
-
-    bu_log_cmd_info cat "$tmp_file"
-    __bu_spawn_tmux_resolve_split_mode "$split_mode" || return 1
-    local tmux_subcommand=("${BU_RET[@]}")
-    local pane_id
-    pane_id=$(tmux "${tmux_subcommand[@]}" bash --init-file "$driver_file")
-    local discarded
-    # shellcheck disable=SC2034
-    read -r discarded <"$fifo_file"
-    rm -f "$fifo_file"
-    BU_RET=$pane_id
+    BU_RET=(tmux "${tmux_args[@]}" bash --init-file)
 }
 
 bu_spawn_tmux_stop_commands()
@@ -151,104 +100,6 @@ bu_spawn_tmux_stop_join_commands()
     done
     bu_spawn_tmux_stop_commands "${to_stop[@]}"
     bu_spawn_tmux_join_commands "${to_join[@]}"
-}
-
-bu_spawn_tmux_command()
-{
-    local split_mode=$1
-    shift
-    local tmp_file
-    tmp_file=$(mktemp "$BU_TMP_DIR"/tmux.XXXXXXXXXX) || return 1
-
-    {
-        echo rm "$tmp_file"
-        __bu_spawn_tmux_header "$1"
-        printf '%q ' "$@"
-        echo
-    } > "$tmp_file"
-
-    __bu_spawn_tmux_resolve_split_mode "$split_mode" || return 1
-    local tmux_subcommand=("${BU_RET[@]}")
-    local pane_id
-    pane_id=$(tmux "${tmux_subcommand[@]}" bash --init-file "$tmp_file")
-    BU_RET=$pane_id
-}
-
-bu_spawn_tmux_joinable_command()
-{
-    local split_mode=$1
-    shift
-    local command=$1
-    local tmp_file
-    tmp_file=$(mktemp "$BU_TMP_DIR"/tmux.XXXXXXXXXX) || return 1
-    local fifo_file=$tmp_file.fifo
-    mkfifo "$fifo_file" || return 1
-    local ret_file=$tmp_file.ret
-    : > "$ret_file" || return 1
-    local ret_file_fd
-    exec {ret_file_fd}<"$ret_file"
-    {
-        __bu_spawn_tmux_joinable_header "$command" "$tmp_file"
-        printf '%q ' "$@"
-        __bu_spawn_tmux_joinable_footer
-    } > "$tmp_file"
-    __bu_spawn_tmux_joinable_driver "$tmp_file" "$split_mode"
-    local pane_id=$BU_RET
-    BU_RET=("$pane_id" "$ret_file_fd")
-}
-
-bu_spawn_tmux_function()
-{
-    local split_mode=$1
-    shift
-    local tmp_file
-    tmp_file=$(mktemp "$BU_TMP_DIR"/tmux.XXXXXXXXXX) || return 1
-    bu_basename "$tmp_file"
-    local random_part=${BU_RET#tmux.}
-    {
-        echo rm "$tmp_file"
-        __bu_spawn_tmux_header
-        echo 'function func_'"$random_part"'() {'
-        cat /dev/stdin
-        echo '}'
-        echo 'func_'"$random_part"
-    } > "$tmp_file"
-
-    __bu_spawn_tmux_resolve_split_mode "$split_mode" || return 1
-    local tmux_subcommand=("${BU_RET[@]}")
-    local pane_id
-    pane_id=$(tmux "${tmux_subcommand[@]}" bash --init-file "$tmp_file")
-    BU_RET=$pane_id
-}
-
-bu_spawn_tmux_joinable_function()
-{
-    local split_mode=$1
-    shift
-    local tmp_file
-    tmp_file=$(mktemp "$BU_TMP_DIR"/tmux.XXXXXXXXXX) || return 1
-    local fifo_file=$tmp_file.fifo
-    mkfifo "$fifo_file" || return 1
-    local ret_file=$tmp_file.ret
-    : > "$ret_file" || return 1
-    local ret_file_fd
-    exec {ret_file_fd}<"$ret_file"
-
-    bu_basename "$tmp_file"
-
-    local random_part=${BU_RET#tmux.}
-    {
-        __bu_spawn_tmux_joinable_header "" "$tmp_file"
-        echo 'function func_'"$random_part"'() {'
-        cat /dev/stdin
-        echo '}'
-        echo 'func_'"$random_part"
-        __bu_spawn_tmux_joinable_footer
-    } > "$tmp_file"
-
-    __bu_spawn_tmux_joinable_driver "$tmp_file"
-    local pane_id=$BU_RET
-    BU_RET=("$pane_id" "$ret_file_fd")
 }
 
 BU_TMUX_PANES_FILE=$BU_PROC_TMP_DIR/tmux_panes.txt
@@ -319,6 +170,7 @@ bu_spawn()
         shift "$shift_by"
     done
     local remaining_options=("$@")
+    local command=${remaining_options[0]}
 
     if "$is_split_opposite"
     then
@@ -344,58 +196,105 @@ bu_spawn()
     local exit_code=0
 
     local pane_id=
+
+
+    local tmp_file
+    tmp_file=$(mktemp "$BU_TMP_DIR"/tmux.XXXXXXXXXX) || return 1
+    local fifo_file=$tmp_file.fifo
+    local ret_file=$tmp_file.ret
+    local ret_file_fd=
+    local driver_file=$tmp_file.driver.sh
+    if "$is_joinable"
+    then
+        mkfifo "$fifo_file" || return 1
+        : > "$ret_file" || return 1
+        exec {ret_file_fd}<"$ret_file" || return 1
+    fi
+
+    local random_part
     if "$is_function"
     then
+        bu_basename "$tmp_file"
+        random_part=${BU_RET#tmux.}
+    fi
+
+    {
+        echo 'rm '"$tmp_file"
+        __bu_spawn_tmux_header "$command"
         if "$is_joinable"
         then
-            bu_spawn_tmux_joinable_function "$split_mode" "${remaining_options[@]}"
-            pane_id=$BU_RET
-            if "$is_wait" && "$is_scoped"
-            then
-                bu_scope_add_cleanup bu_spawn_tmux_join_commands "${BU_RET[1]}"
-            elif "$is_wait"
-            then
-                if ! bu_spawn_tmux_join_commands "${BU_RET[1]}"
-                then
-                    exit_code=1
-                fi
-            elif "$is_scoped"
-            then
-                bu_scope_add_cleanup bu_spawn_tmux_stop_join_commands "${BU_RET[@]}"
-            fi
+        # echo 1 first to account for possible Ctrl-C
+        echo 'echo 1 >'"$ret_file"
+        echo 'exec {RET_FILE_FD}>'"$ret_file"
+        echo 'rm '"$ret_file"
+        echo 'bu_exit_handler_setup_no_e'
+        echo 'bu_scope_push'
+        # shellcheck disable=SC2016
+        echo 'bu_sync_acquire_fd "$RET_FILE_FD"'
+        # shellcheck disable=SC2016
+        echo 'bu_scope_cleanup bu_release_fd "$RET_FILE_FD"'
+        echo 'echo >'"$fifo_file"
+        fi
+
+        if "$is_function"
+        then
+        echo 'function func_'"$random_part"'() {'
+        cat /dev/stdin
+        echo '}'
+        echo 'func_'"$random_part"
         else
-            bu_spawn_tmux_function "$split_mode" "${remaining_options[@]}"
-            pane_id=$BU_RET
-            if "$is_scoped"
-            then
-                bu_scope_add_cleanup bu_spawn_tmux_stop_commands "$BU_RET"
-            fi
+        printf '%q ' "${remaining_options[@]}"
+        fi
+
+        echo
+        if "$is_joinable"
+        then
+        # shellcheck disable=SC2016
+        echo 'echo $? >&"$RET_FILE_FD"'
+        echo 'bu_scope_pop'
+        fi
+    } > "$tmp_file"
+
+    if ! "$is_joinable"
+    then
+        __bu_spawn_tmux_resolve_split_mode "$split_mode" || return 1
+        pane_id=$("${BU_RET[@]}" "$tmp_file")
+    else
+        {
+            echo 'rm '"$driver_file"
+            echo . /etc/profile
+            echo . "$HOME"/.bashrc
+            echo bash "$tmp_file"
+        } > "$driver_file"
+
+        bu_log_cmd_info cat "$tmp_file"
+        __bu_spawn_tmux_resolve_split_mode "$split_mode" || return 1
+        pane_id=$("${BU_RET[@]}" "$driver_file")
+        local discarded
+        # shellcheck disable=SC2034
+        read -r discarded <"$fifo_file"
+        rm -f "$fifo_file"
+    fi
+
+    if ! "$is_joinable"
+    then
+        if "$is_scoped"
+        then
+            bu_scope_add_cleanup bu_spawn_tmux_stop_commands "$pane_id"
         fi
     else
-        if "$is_joinable"
+        if "$is_wait" && "$is_scoped"
         then
-            bu_spawn_tmux_joinable_command "$split_mode" "${remaining_options[@]}"
-            pane_id=$BU_RET
-            if "$is_wait" && "$is_scoped"
+            bu_scope_add_cleanup bu_spawn_tmux_join_commands "$ret_file_fd"
+        elif "$is_wait"
+        then
+            if ! bu_spawn_tmux_join_commands "$ret_file_fd"
             then
-                bu_scope_add_cleanup bu_spawn_tmux_join_commands "${BU_RET[1]}"
-            elif "$is_wait"
-            then
-                if ! bu_spawn_tmux_join_commands "${BU_RET[1]}"
-                then
-                    exit_code=1
-                fi
-            elif "$is_scoped"
-            then
-                bu_scope_add_cleanup bu_spawn_tmux_stop_join_commands "${BU_RET[1]}"
+                exit_code=1
             fi
-        else
-            bu_spawn_tmux_command "$split_mode" "${remaining_options[@]}"
-            pane_id=$BU_RET
-            if "$is_scoped"
-            then
-                bu_scope_add_cleanup bu_spawn_tmux_stop_commands "$BU_RET"
-            fi
+        elif "$is_scoped"
+        then
+            bu_scope_add_cleanup bu_spawn_tmux_stop_join_commands "$pane_id" "$ret_file_fd"
         fi
     fi
 
@@ -403,6 +302,14 @@ bu_spawn()
     printf '%s\n' "$pane_id" >> "$BU_TMUX_PANES_FILE"
     bu_sync_release_fd "$BU_TMUX_PANES_FILE"
     bu_log_debug "exit_code[$exit_code]"
+
+    if "$is_joinable"
+    then
+        BU_RET=("$pane_id" "$ret_file_fd")
+    else
+        BU_RET=("$pane_id")
+    fi
+
     return "$exit_code"    
 }
 
